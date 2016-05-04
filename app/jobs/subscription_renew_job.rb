c36de830153f9837cafc3bd4355fa7b51928da53
keegnotrub <keegnotrub@icloud.com>
2016-05-04 14:23:31 -0500

    Combine renew/charge jobs

--- app/jobs/subscription_renew_job.rb
+++ app/jobs/subscription_renew_job.rb
@@ -11,8 +11,17 @@ class SubscriptionRenewJob < ApplicationJob
   def perform(start_at_tick, end_at_tick)
     start_at, end_at = Time.at(start_at_tick), Time.at(end_at_tick)
     time_range = start_at...end_at
+
+    perform_renewal_for_range(time_range)
+    perform_charges_for_range(time_range)
+
     run_again_at = end_at + self.class::INTERVAL
-    
+    SubscriptionRenewJob.set(wait_until: run_again_at).perform_later(end_at.to_f, run_again_at.to_f)
+  end
+
+  private
+
+  def perform_renewal_for_range(time_range)
     Subscription.unarchived.renewable.ending(time_range).includes(:plan).each do |subscription|
       if subscription.cancel_at_period_end?
         subscription.cancel!
@@ -20,8 +29,35 @@ class SubscriptionRenewJob < ApplicationJob
         subscription.renew!
       end
     end
+  end
 
-    SubscriptionChargeJob.perform_later(start_at.to_f, end_at.to_f)    
-    SubscriptionRenewJob.set(wait_until: run_again_at).perform_later(end_at.to_f, run_again_at.to_f)
+  def perform_charges_for_range(time_range)
+    payment_charges = Payment::Charge.since(time_range.last).fetch
+
+    Subscription.unarchived.chargable.starting(time_range).includes(:plan, :user).each do |subscription|
+      if payment_charge = payment_charges.find_by_subscription(subscription)
+        subscription.transaction do
+          charge = subscription.charges.create_for_payment_charge!(payment_charge)
+          process_charge_for_subscription(charge, subscription)
+        end
+      elsif card = subscription.user.default_card
+        subscription.transaction do
+          charge = subscription.charges.create_for_card!(card)
+          process_charge_for_subscription(charge, subscription)
+        end
+      else
+        process_charge_for_subscription(nil, subscription)
+      end
+    end
+  end
+
+  def process_charge_for_subscription(charge, subscription)
+    if charge.try(:paid?)
+      subscription.paid!
+      ChargeMailer.paid(charge).deliver_later
+    else
+      subscription.cancel!
+      SubscriptionMailer.unpaid(subscription).deliver_later
+    end
   end
 end
